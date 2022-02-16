ca830067ee8a7831385da02b1a2ce618901135f7
cyberKnight17 <dingtongwang@outlook.com>
2022-02-16 15:51:40 -0500

    this keyword

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
 console.log("your code goes here");
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+    describe() {
+        console.log("this is ", this.name, "department");
+    }
+}
+const departmentAccounting = new Department("accounting");
+console.log(departmentAccounting);
+departmentAccounting.describe();
+// const copyDepartmentAccounting = { describe: departmentAccounting.describe };
+// copyDepartmentAccounting.describe();
--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
 console.log("your code goes here");
+
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  describe(this: Department) {
+    console.log("this is ", this.name, "department");
+  }
+}
+
+const departmentAccounting = new Department("accounting");
+
+console.log(departmentAccounting);
+
+departmentAccounting.describe();
+
+// const copyDepartmentAccounting = { describe: departmentAccounting.describe };
+
+// copyDepartmentAccounting.describe();
